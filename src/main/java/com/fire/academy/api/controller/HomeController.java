cd0f9134ab08d6d784705329e9f1a03db292f4d8
Diego Barrio H <diegobarrioh@gmail.com>
2021-12-12 13:42:04 +0100

    Preparados los entornos dev y prod y el home de UI, index de bootstrap 5

---
+++ src/main/java/com/fire/academy/api/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.fire.academy.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+}
